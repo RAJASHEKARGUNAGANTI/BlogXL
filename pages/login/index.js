dbc89ee47355341abcf5dcc365b810050328f2aa
Rajashekar Gunaganti <gunagantirajashekar@gmail.com>
2024-05-29 20:50:29 +0530

    metadata for login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,6 +4,12 @@ import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
+import Head from "next/head"; // Import the Head component
+
+export const metadata = {
+  title: "Login",
+  description: "Login to exceel the BlogXL",
+};
 
 const Login = () => {
   const {data: session} = useSession()
@@ -16,6 +22,8 @@ const Login = () => {
   })
   
   return <>
+  <title>{metadata.title} - BlogXL</title>
+        <meta name="description" content={metadata.description} />
   <section>
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-10 relative z-10">
   <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text:6xl">
