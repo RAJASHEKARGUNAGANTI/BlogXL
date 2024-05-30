6c1d0136ed80273b9a8bdc566661b0db69486d27
Rajashekar Gunaganti <gunagantirajashekar@gmail.com>
2024-05-30 09:07:28 +0530

    login head tag is set

--- pages/login/index.js
+++ pages/login/index.js
@@ -22,8 +22,10 @@ const Login = () => {
   })
   
   return <>
+  <Head>
   <title>{metadata.title} - BlogXL</title>
         <meta name="description" content={metadata.description} />
+  </Head>
   <section>
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-10 relative z-10">
   <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text:6xl">
